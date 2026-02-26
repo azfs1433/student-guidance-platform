d17aa375216c829190219163c4ed3c4623234b85
azfs1433 <azfs1433@gmail.com>
2026-02-27 01:19:33 +0300

    Add files via upload

---
+++ config/settings.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+import os
+import dj_database_url
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY","change-me")
+DEBUG = False
+ALLOWED_HOSTS = ["*"]
+
+INSTALLED_APPS = [
+'django.contrib.admin',
+'django.contrib.auth',
+'django.contrib.contenttypes',
+'django.contrib.sessions',
+'django.contrib.messages',
+'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+'django.middleware.security.SecurityMiddleware',
+'whitenoise.middleware.WhiteNoiseMiddleware',
+'django.contrib.sessions.middleware.SessionMiddleware',
+'django.middleware.common.CommonMiddleware',
+'django.middleware.csrf.CsrfViewMiddleware',
+'django.contrib.auth.middleware.AuthenticationMiddleware',
+'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+ROOT_URLCONF='config.urls'
+
+TEMPLATES=[{
+'BACKEND':'django.template.backends.django.DjangoTemplates',
+'DIRS':[BASE_DIR/'templates'],
+'APP_DIRS':True,
+}]
+
+WSGI_APPLICATION='config.wsgi.application'
+
+DATABASES={
+'default': dj_database_url.config(default=f"sqlite:///{BASE_DIR/'db.sqlite3'}")
+}
+
+STATIC_URL='static/'
+STATIC_ROOT=BASE_DIR/'staticfiles'
+
+DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
