aae9f97e7c16b403bcb5bc71714ea900d69598f3
azfs1433 <azfs1433@gmail.com>
2026-02-27 22:47:13 +0300

    Update admin.py

--- guidance/guidance/admin.py
+++ guidance/guidance/admin.py
@@ -1,37 +1,18 @@
 from django.contrib import admin
-from .models import ClassRoom, Student, Referral, ReferralStudent, Intervention
-
+from .models import ClassRoom, Student, Case
 
 @admin.register(ClassRoom)
 class ClassRoomAdmin(admin.ModelAdmin):
-    search_fields = ("name",)
-    list_display = ("name",)
-
+    list_display = ("id", "name")
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ("name", "classroom", "phone")
+    list_display = ("id", "name", "classroom", "phone")
     list_filter = ("classroom",)
-    search_fields = ("name", "phone")
+    search_fields = ("name",)
 
-
-class ReferralStudentInline(admin.TabularInline):
-    model = ReferralStudent
-    extra = 0
-
-
-@admin.register(Referral)
-class ReferralAdmin(admin.ModelAdmin):
-    list_display = ("id", "reason", "created_by", "assigned_to", "created_at")
-    list_filter = ("reason", "created_at")
-    search_fields = ("created_by__username",)
-    inlines = [ReferralStudentInline]
-
-
-@admin.register(Intervention)
-class InterventionAdmin(admin.ModelAdmin):
-    list_display = ("id", "action", "created_by", "created_at")
-    list_filter = ("action", "created_at")
-    search_fields = ("created_by__username", "referral_student__student__name")
+@admin.register(Case)
+class CaseAdmin(admin.ModelAdmin):
+    list_display = ("id", "student", "case_type", "date")
+    list_filter = ("case_type",)
     
-
