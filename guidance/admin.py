2e334023e1c0b7057fe1f6a8f923837d8364b270
azfs1433 <azfs1433@gmail.com>
2026-02-28 00:11:01 +0300

    Fix admin imports / register existing models

--- guidance/admin.py
+++ guidance/admin.py
@@ -5,17 +5,18 @@ from .models import ClassRoom, Student, Case
 @admin.register(ClassRoom)
 class ClassRoomAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
+    search_fields = ("name",)
 
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "national_id", "classroom", "phone")
     list_filter = ("classroom",)
-    search_fields = ("name", "national_id")
+    search_fields = ("name", "national_id", "phone")
 
 
 @admin.register(Case)
 class CaseAdmin(admin.ModelAdmin):
     list_display = ("id", "student", "case_type", "date")
     list_filter = ("case_type", "date")
-    
+    search_fields = ("student__name", "student__national_id", "case_type")
