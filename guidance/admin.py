26ff862455dde669a44a4d6be0a61f5a15943386
azfs1433 <azfs1433@gmail.com>
2026-02-27 23:42:40 +0300

    Register models in admin

--- guidance/admin.py
+++ guidance/admin.py
@@ -1,19 +1,21 @@
 from django.contrib import admin
 from .models import ClassRoom, Student, Case
 
+
 @admin.register(ClassRoom)
 class ClassRoomAdmin(admin.ModelAdmin):
-    search_fields = ("name",)
+    list_display = ("id", "name")
+
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ("name", "classroom", "phone")
+    list_display = ("id", "name", "national_id", "classroom", "phone")
     list_filter = ("classroom",)
-    search_fields = ("name", "phone", "national_id")
+    search_fields = ("name", "national_id")
+
 
 @admin.register(Case)
 class CaseAdmin(admin.ModelAdmin):
     list_display = ("id", "student", "case_type", "date")
     list_filter = ("case_type", "date")
-    search_fields = ("student__name", "description", "action_taken")
     
