36fffacca1043b3c0b184331c071191ab617e7b9
azfs1433 <azfs1433@gmail.com>
2026-02-28 03:39:11 +0300

    Update models.py

--- guidance/models.py
+++ guidance/models.py
@@ -12,12 +12,13 @@ class ClassRoom(models.Model):
 
 class Student(models.Model):
     name = models.CharField(max_length=200)
+    national_id = models.CharField(max_length=20, unique=True)  # <-- أضفناه
     classroom = models.ForeignKey(ClassRoom, on_delete=models.CASCADE, related_name="students")
     phone = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
         return f"{self.name} - {self.classroom.name}"
-
+        
 
 class Referral(models.Model):
     """
